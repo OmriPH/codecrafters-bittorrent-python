ae33f82845f7c372edbcfaf4812f70d76c9f0988
Omri <omri.hermon@gmail.com>
2024-10-04 19:07:46 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,118 +1,14 @@
+import hashlib
+
 import json
 
 import sys
 
-# import bencodepy - available if you need it!
+import bencodepy
 
 # import requests - available if you need it!
 
-
-def decode_part(value, start_index):
-
-    if chr(value[start_index]).isdigit():
-
-        return decode_string(value, start_index)
-
-    elif chr(value[start_index]) == "i":
-
-        return decode_integer(value, start_index)
-
-    elif chr(value[start_index]) == "l":
-
-        return decode_list(value, start_index)
-
-    elif chr(value[start_index]) == "d":
-
-        return decode_dict(value, start_index)
-
-    else:
-
-        raise NotImplementedError(
-
-            "Only strings and integers are supported at the moment"
-
-        )
-
-
-def decode_string(bencoded_value, start_index):
-
-    if not chr(bencoded_value[start_index]).isdigit():
-
-        raise ValueError("Invalid encoded string", bencoded_value, start_index)
-
-    bencoded_value = bencoded_value[start_index:]
-
-    first_colon_index = bencoded_value.find(b":")
-
-    if first_colon_index == -1:
-
-        raise ValueError("Invalid encoded value")
-
-    length = int(bencoded_value[:first_colon_index])
-
-    word_start = first_colon_index + 1
-
-    word_end = first_colon_index + length + 1
-
-    return bencoded_value[word_start:word_end], start_index + word_end
-
-
-def decode_integer(bencoded_value, start_index):
-
-    if chr(bencoded_value[start_index]) != "i":
-
-        raise ValueError("Invalid encoded integer",
-                         bencoded_value, start_index)
-
-    bencoded_value = bencoded_value[start_index:]
-
-    end_marker = bencoded_value.find(b"e")
-
-    if end_marker == -1:
-
-        raise ValueError("Invalid encoded integer", bencoded_value)
-
-    return int(bencoded_value[1:end_marker]), start_index + end_marker + 1
-
-
-def decode_list(bencoded_value, start_index):
-
-    if chr(bencoded_value[start_index]) != "l":
-
-        raise ValueError("Invalid encoded list", bencoded_value, start_index)
-
-    current_index = start_index + 1
-
-    values = []
-
-    while chr(bencoded_value[current_index]) != "e":
-
-        value, current_index = decode_part(bencoded_value, current_index)
-
-        values.append(value)
-
-    return values, current_index + 1
-
-
-def decode_dict(bencoded_value, start_index):
-
-    if chr(bencoded_value[start_index]) != "d":
-
-        raise ValueError("Invalid encoded dict", bencoded_value, start_index)
-
-    current_index = start_index + 1
-
-    values = {}
-
-    while chr(bencoded_value[current_index]) != "e":
-
-        key, current_index = decode_string(bencoded_value, current_index)
-
-        value, current_index = decode_part(bencoded_value, current_index)
-
-        values[key.decode()] = value
-
-    return values, current_index
+bc = bencodepy.Bencode(encoding="utf-8")
 
 # Examples:
 
@@ -125,7 +21,7 @@ def decode_dict(bencoded_value, start_index):
 
 def decode_bencode(bencoded_value):
 
-    return decode_part(bencoded_value, 0)[0]
+    return bc.decode(bencoded_value)
 
 
 def main():
@@ -152,23 +48,23 @@ def main():
 
             raise TypeError(f"Type not serializable: {type(data)}")
 
-        # Uncomment this block to pass the first stage
-
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
 
     elif command == "info":
 
-        file_name = sys.argv[2]
+        with open(sys.argv[2], "rb") as torrent_file:
 
-        with open(file_name, "rb") as torrent_file:
+            info = torrent_file.read()
 
-            bencoded_content = torrent_file.read()
+        info_dict = bencodepy.Bencode().decode(info)
 
-        torrent = decode_bencode(bencoded_content)
+        info_hash = hashlib.sha1(bencodepy.encode(info_dict[b"info"]))
 
-        print("Tracker URL:", torrent["announce"].decode())
+        print(f'Tracker URL: {info_dict[b"announce"].decode()}')
 
-        print("Length:", torrent["info"]["length"])
+        print(f'Length: {info_dict[b"info"][b"length"]}')
+
+        print(f"Info Hash: {info_hash.hexdigest()}")
 
     else:
 
