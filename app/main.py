93b7dfa48f54ca97292d7e9b2465c9a1eb06b059
Omri <omri.hermon@gmail.com>
2024-10-04 19:12:46 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -66,6 +66,11 @@ def main():
 
         print(f"Info Hash: {info_hash.hexdigest()}")
 
+        print(f"Piece Length: {info_dict[b"info"][b"piece length"]}")
+
+        for i in range(0, len(info_dict[b"info"][b"pieces"]), 20):
+            print(info_dict[b"info"][b"pieces"][i: i + 20].hex())
+
     else:
 
         raise NotImplementedError(f"Unknown command {command}")
